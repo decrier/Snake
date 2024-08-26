b46a8fa0a67c8a81c6c0a793d36fa9046608803a
Kolja <decrier@ukr.net>
2024-08-26 11:12:19 +0200

    1.1

--- game.js
+++ game.js
@@ -2,3 +2,12 @@
 
 let ansi = require('ansi');
 let cursor = ansi(process.stdout);
+
+let width = 40;
+let height = 20;
+
+function drawHorizontalLine(col, row, length) {
+    for (let i = 0; i < length; i++) {
+        cursor.goto(col + i, row).write(' ');        
+    }
+}
